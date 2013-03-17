f83d25de39d8f41d0e3acd4e05340b11e69e7909
AdamT <atal421@yahoo.com>
2013-03-17 16:38:24 -0400

    add verbs, and sinatra-contrib

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'https://rubygems.org'
 
-gem 'sinatra', '~> 1.4.1'
+# newer sinatra not compatible with sinatra-contrib
+#gem 'sinatra', '~> 1.4.1'
+gem 'sinatra', git: 'https://github.com/juanpastas/sinatra.git'
 #gem 'unicorn'
 gem 'json', '~> 1.7.7'
 gem 'mongoid', '~> 3.1.2'
+gem 'sinatra-contrib', git: 'https://github.com/sinatra/sinatra-contrib.git'
--- app.rb
+++ app.rb
@@ -1,21 +1,74 @@
 require 'sinatra'
+require "sinatra/namespace"
 require 'json'
 require 'mongoid'
+
 require './models/post'
 require './models/user'
 require './models/comment'
 
 Mongoid.load!("config/mongoid.yml")
 
+namespace "/v1" do
+  get "/" do
+    index_response
+  end
 
-get "/" do
-  index_response
+  get "/blog" do
+    "hi"
+  end
 end
 
 get "/index.json" do
   index_response
 end
 
+# show all posts
+get "/posts" do
+end
+
+# create a post
+post "/posts/create" do
+end
+
+# delete a post
+delete "/posts/:post_id/delete" do
+end
+
+# show a post
+get "/posts/:post_id" do
+end
+
+# comment on post
+post "/posts/:post_id/comments/create" do
+end
+
+# show post comment
+get "/posts/:post_id/comments/:comment_id" do
+end
+
+# show all users
+get "/users" do
+end
+
+# show user
+get "/users/:id" do
+end
+
+# create user
+post "/users/create" do
+end
+
+# show user posts
+get "/users/:user_id/posts" do
+end
+
+# show user comments
+get "/users/:user_id/comments" do
+end
+
+
+# quick seed
 get "/seed" do
   User.create(full_name: "John Smith",
               email: "a@b.c",
@@ -24,6 +77,7 @@ get "/seed" do
   redirect "/"
 end
 
+# quick clean
 get "/clean" do
   Comment.delete_all
   Post.delete_all
@@ -32,9 +86,13 @@ get "/clean" do
   redirect "/"
 end
 
+# show help page
 get "/help" do
   # TODO
   # load help page
+end
+
+options "/" do
 end
 
 def index_response
